dec12bb7c38218233f7f2e09a46b9f5bb30aa59f
= <=>
2022-06-29 09:00:22 +0700

    Belajar Tipe Data Integer

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h1>Daftar Menu</h1>";
+print "<li>Ayam Bakar = Rp.13000</li>";
+print "<li>Ayam Goreng = Rp.15000</li>";
+print "<li>Ayam Sayur = Rp.13000</li>";
+print "<li>Tempe Goreng = Rp.1000</li>";
+print "<li>Tahu Goreng = Rp.1000</li>";
+print "<li>Soto = Rp.3000</li>";
+print "<li>Nasi Putih = Rp.5000</li>";
+print "<li>Es Teh = Rp.4000</li>";
+print "<li>Es Tebu = Rp.5000</li>";
+
+?>
