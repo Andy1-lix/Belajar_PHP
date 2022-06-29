4f8026bfad20b42133a56e94a54be7514bb5a527
= <=>
2022-06-29 08:59:08 +0700

    Tugas 1 PHP

---
+++ Tugas1/Tugas_2.php
@@ -0,0 +1,41 @@
+<?php 
+echo "<h1>Daftar Menu</h1>";
+echo "<table border=1>";
+print "<tr>";
+print "<th>No</th>";
+print "<th>Nama Menu</th>";
+print "<th>harga</th>";
+print "</tr>";
+
+print "<tr>";
+print "<td>1</td>";
+print "<td>Cireng</td>";
+print "<td>Rp 1000</td>";
+print "</tr>";
+
+print "<tr>";
+print "<td>2</td>";
+print "<td>Es Duren</td>";
+print "<td>Rp 15000</td>";
+print "</tr>";
+
+print "<tr>";
+print "<td>3</td>";
+print "<td>Es Milo</td>";
+print "<td>Rp 20000</td>";
+print "</tr>";
+
+print "<tr>";
+print "<td>4</td>";
+print "<td>Es Teh</td>";
+print "<td>Rp 5000</td>";
+print "</tr>";
+
+print "<tr>";
+print "<td>5</td>";
+print "<td>Piscok</td>";
+print "<td>Rp 1000</td>";
+print "</tr>";
+echo "</table>";
+
+?>
