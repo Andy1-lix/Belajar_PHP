3314711898c9aee73fedaf8687affade9a700150
= <=>
2022-06-29 13:21:33 +0700

    Tugas 4 PHP

---
+++ Tugas4/Tugas2.php
@@ -0,0 +1,9 @@
+<?php 
+$jumlah_saluran = 523;
+$jumlah_terpakai = 8891;
+
+$rata2_pemakaian = $jumlah_terpakai/$jumlah_saluran;
+
+echo "Jadi rata rata air yang oleh cabang dalam 1 minggu yakni $rata2_pemakaian liter"
+
+?>
---
+++ Tugas4/Tugas_1.php
@@ -0,0 +1,11 @@
+<?php 
+
+$harga_beli = 8000;
+$harga_jual = 7500;
+$untung = 300;
+
+$total_kambing = ($harga_jual / $untung) ;
+// $fix_total = $harga_beli / $total_kambing;
+echo "Jadi Total Kambing yang dibeli adalah $total_kambing";
+
+?>
