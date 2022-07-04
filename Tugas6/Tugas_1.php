5f2e44d39bb697ca69c02937286163a56de7c492
= <=>
2022-07-04 09:19:13 +0700

    Tugas 6 PHP

---
+++ Tugas6/Tugas_1.php
@@ -0,0 +1,11 @@
+<?php 
+$bintan = 0;
+$aji = 0;
+$habib = 23;
+
+echo "Usia Habib = $habib <br>";
+$usia_aji = $habib + 4;
+echo "Usia Aji = $usia_aji<br>";
+$usia_bintan = $usia_aji + 3;
+echo "Usia Bintan = $usia_bintan<br>";
+?>
---
+++ Tugas6/Tugas_2.php
@@ -0,0 +1,10 @@
+<?php
+
+$jambukg = 15000;
+$dus = 6 * $jambukg;
+$dusbekas = 4000;
+
+$total = $dus + $dusbekas;
+
+echo "Jadi, untuk penjualan semua jambu yakni Rp.$total";
+?>
